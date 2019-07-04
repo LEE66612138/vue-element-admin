4a5f08dc44735023f11b3a2f41741a268744e5f7
LiFuyao <1131197832@qq.com>
2019-07-04 17:46:26 +0800

    空

---
+++ src/views/dashboard/administrator-center.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>这是管理中心界面</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/dashboard/administrator.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>这是管理员界面</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
